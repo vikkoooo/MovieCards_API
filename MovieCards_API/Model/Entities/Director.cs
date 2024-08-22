ec988e947ed3a1a0ba009bb4db38fba08e8ad4a7
vikkoooo <vikkolundberg@gmail.com>
2024-08-22 16:33:13 +0200

    add relationship 1 to many movie director

--- MovieCards_API/Model/Entities/Director.cs
+++ MovieCards_API/Model/Entities/Director.cs
@@ -6,5 +6,8 @@
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public DateTime DateOfBirth { get; set; }
+
+		// Relationships
+		public ICollection<Movie> Movies { get; set; }
 	}
 }
--- MovieCards_API/Model/Entities/Movie.cs
+++ MovieCards_API/Model/Entities/Movie.cs
@@ -8,5 +8,9 @@
 		public int Rating { get; set; }
 		public DateTime ReleaseDate { get; set; }
 		public string Description { get; set; }
+
+		// Relationships
+		public int DirectorId { get; set; }
+		public Director Director { get; set; }
 	}
 }
