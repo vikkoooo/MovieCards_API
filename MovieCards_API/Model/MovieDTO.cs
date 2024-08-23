f8d0646f6a3b4725c2a0baa646113719abce32b5
vikkoooo <vikkolundberg@gmail.com>
2024-08-23 12:37:35 +0200

    create movie dto

---
+++ MovieCards_API/Model/MovieDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieCards_API.Model
+{
+	public class MovieDTO
+	{
+		public string Title { get; set; }
+		public int Rating { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string Description { get; set; }
+		public string DirectorName { get; set; }
+		public ICollection<string> ActorNames { get; set; }
+		public ICollection<string> GenreNames { get; set; }
+	}
+}
